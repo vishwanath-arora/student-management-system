c31eee1817f7b08352a91cbc58025d305033e6b9
vishwanath-arora <vishwanath.arora.dev@gmail.com>
2026-09-08 00:20:09 +0530

    Add Student class with properties and constructor

---
+++ Student.java
@@ -0,0 +1,24 @@
+public class Student {
+
+    private int id;
+    private String name;
+    private String course;
+
+    public Student(int id, String name, String course) {
+        this.id = id;
+        this.name = name;
+        this.course = course;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getCourse() {
+        return course;
+    }
+}
